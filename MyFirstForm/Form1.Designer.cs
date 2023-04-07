fa637317574b9731e02825ccebc1adc029686b72
Chirath Kavindu <95146759+Chira2000@users.noreply.github.com>
2023-04-07 21:47:49 +0530

    Add project files.

---
+++ MyFirstForm/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MyFirstForm
+{
+    partial class Welcome
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btnok = new Button();
+            btnclear = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 20);
+            label1.TabIndex = 0;
+            label1.Text = "First Name";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(13, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(544, 27);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Last Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(12, 98);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(544, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            label3.Click += label3_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(btnok, 0, 0);
+            tableLayoutPanel1.Controls.Add(btnclear, 1, 0);
+            tableLayoutPanel1.Location = new Point(12, 400);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(544, 38);
+            tableLayoutPanel1.TabIndex = 5;
+            // 
+            // btnok
+            // 
+            btnok.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnok.Location = new Point(3, 3);
+            btnok.Name = "btnok";
+            btnok.Size = new Size(266, 29);
+            btnok.TabIndex = 0;
+            btnok.Text = "OK";
+            btnok.UseVisualStyleBackColor = true;
+            btnok.Click += button1_Click;
+            // 
+            // btnclear
+            // 
+            btnclear.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnclear.Location = new Point(275, 3);
+            btnclear.Name = "btnclear";
+            btnclear.Size = new Size(266, 29);
+            btnclear.TabIndex = 1;
+            btnclear.Text = "Clear";
+            btnclear.UseVisualStyleBackColor = true;
+            btnclear.Click += button2_Click;
+            // 
+            // Welcome
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(573, 450);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Welcome";
+            Text = "Welcome";
+            Load += Welcome_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btnok;
+        private Button btnclear;
+    }
+}
---
+++ MyFirstForm/Form1.cs
@@ -0,0 +1,50 @@
+namespace MyFirstForm
+{
+    public partial class Welcome : Form
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String firstName;
+            firstName = textBox1.Text;
+            String lastName;
+            lastName = textBox2.Text;
+            label3.Text = "Hello " + firstName + " " + lastName;
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label3.Text = "";
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Welcome_Load(object sender, EventArgs e)
+        {
+            label3.Text = "";
+        }
+    }
+}
